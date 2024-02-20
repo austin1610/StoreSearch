cb6e790e028ec195859bb2d954e07f574ba92c64
austin1610 <austinsparks09@gmail.com>
2024-02-20 15:16:29 -0600

    Added code to dismiss keyboard after a search until the search bar is pressed again

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -22,6 +22,7 @@ class SearchViewController: UIViewController {
 // MARK: - Search Bar Delegate
 extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
         searchResults = []
         for i in 0...2 {
             searchResults.append(
