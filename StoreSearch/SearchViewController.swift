8ebbb677e5ceb7101c234269709990f751ba11b4
austin1610 <austinsparks09@gmail.com>
2024-02-22 13:55:53 -0600

    set up loading cell and made code asynchronous

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -13,11 +13,13 @@ class SearchViewController: UIViewController {
 
   var searchResults = [SearchResult]()
   var hasSearched = false
+  var isLoading = false
     
   struct TableView {
       struct CellIdentifiers {
           static let searchResultCell = "SearchResultCell"
           static let nothingFoundCell = "NothingFoundCell"
+          static let loadingCell = "LoadingCell"
       }
   }
 
@@ -28,6 +30,8 @@ class SearchViewController: UIViewController {
       tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.searchResultCell)
       cellNib = UINib(nibName: TableView.CellIdentifiers.nothingFoundCell, bundle: nil)
       tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.nothingFoundCell)
+      cellNib = UINib(nibName: TableView.CellIdentifiers.loadingCell, bundle: nil)
+      tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.loadingCell)
       searchBar.becomeFirstResponder()
   }
     
@@ -79,15 +83,27 @@ extension SearchViewController: UISearchBarDelegate {
   func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
       if !searchBar.text!.isEmpty {
           searchBar.resignFirstResponder()
+          isLoading = true
+          tableView.reloadData()
           
           hasSearched = true
           searchResults = []
           
-          let url = iTunesURL(searchText: searchBar.text!)
-          print("URL: '\(url)'")
-          if let data = performStoreRequest(with: url) {
-              searchResults = parse(data: data)
-              searchResults.sort { $0 < $1 }
+          let queue = DispatchQueue.global()
+          let url = self.iTunesURL(searchText: searchBar.text!)
+          
+          queue.async {
+              
+              if let data = self.performStoreRequest(with: url) {
+                  self.searchResults = self.parse(data: data)
+                  self.searchResults.sort(by: <)
+                  
+                  DispatchQueue.main.async {
+                      self.isLoading = false
+                      self.tableView.reloadData()
+                  }
+                  return
+              }
           }
           tableView.reloadData()
       }
@@ -101,7 +117,9 @@ extension SearchViewController: UISearchBarDelegate {
 // MARK: - Table View Delegate
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    if !hasSearched {
+    if isLoading {
+      return 1
+    } else if !hasSearched {
       return 0
     } else if searchResults.count == 0 {
       return 1
@@ -111,7 +129,13 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    if searchResults.count == 0 {
+    if isLoading {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.loadingCell, for: indexPath)
+          
+        let spinner = cell.viewWithTag(100) as! UIActivityIndicatorView
+        spinner.startAnimating()
+        return cell
+    } else if searchResults.count == 0 {
         return tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.nothingFoundCell, for: indexPath)
     } else {
         let cell = tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.searchResultCell, for: indexPath) as! SearchResultCell
@@ -135,7 +159,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
   }
 
   func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-    if searchResults.count == 0 {
+    if searchResults.count == 0 || isLoading {
       return nil
     } else {
       return indexPath
