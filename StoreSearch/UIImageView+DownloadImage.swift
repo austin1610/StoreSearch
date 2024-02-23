0a3af63d9a26761fae442f5cb360f4b45deb9b5d
austin1610 <austinsparks09@gmail.com>
2024-02-22 20:18:11 -0600

    stage all changes

--- StoreSearch/UIImageView+DownloadImage.swift
+++ StoreSearch/UIImageView+DownloadImage.swift
@@ -5,4 +5,27 @@
 //  Created by Austin Sparks on 2/22/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    func loadImage(url: URL) -> URLSessionDownloadTask {
+        let session = URLSession.shared
+ 
+        let downloadTask = session.downloadTask(with: url) {
+            [weak self] url, _, error in
+ 
+            if error == nil, let url = url,
+               let data = try? Data(contentsOf: url), // 3
+               let image = UIImage(data: data) {
+ 
+                DispatchQueue.main.async {
+                    if let weakSelf = self {
+                        weakSelf.image = image
+                    }
+                }
+            }
+        }
+        downloadTask.resume()
+        return downloadTask
+    }
+}
