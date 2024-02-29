27038f885702f8cb89c4bd84a2dbb51bc9d19f9e
austin1610 <austinsparks09@gmail.com>
2024-02-29 00:00:24 -0600

    Added files and a bunch of other code to support different languages

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -187,7 +187,7 @@ class LandscapeViewController: UIViewController {
 
   private func showNothingFoundLabel() {
     let label = UILabel(frame: CGRect.zero)
-    label.text = "Nothing Found"
+    label.text = NSLocalizedString("Nothing Found", comment: "Search results: Nothing Found")
     label.textColor = UIColor.label
     label.backgroundColor = UIColor.clear
 
